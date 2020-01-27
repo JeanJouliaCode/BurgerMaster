406ef0aaacd7658be1fc39857917dd5cb494eabd
JeanJouliaCode <jjeeaann2015@gmail.com>
2020-01-27 14:10:37 +0100

    falling ingredient

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function startGame() {
-    var burger = new Burger();
-    var order = document.getElementById('order')
-    order.addEventListener('click', burger.prepare(["bredBottom","meat","bredTop"]));
+  var burger = new Burger();
+  var order = document.getElementById("order");
+  order.addEventListener(
+    "click",
+    burger.prepare(
+      ["bredBottom", "ketchup", "cheese", "meat", "egg", "tomato", "bredTop"],
+      10
+    )
+  );
 }
-
-
-
-
-
-
-
-
--- serving.js
+++ serving.js
@@ -1,43 +1,77 @@
-var Burger = class Burger{
+var Burger = class Burger {
+  constructor() {
+    this.servingDiv = document.getElementById("serving");
+    this.burgerElement = [];
+    this.positionInfo = {
+      bredTop: { width: "53%", left: "22%", offset: 0 },
+      bredBottom: { width: "47%", left: "23%", offset: 0 },
+      bredTopBlack: { width: "50%", left: "25%", offset: 0 },
+      bredBottomBlack: { width: "50%", left: "25%", offset: 0 },
+      beacon: { width: "50%", left: "25%", offset: 0 },
+      cheese: { width: "50%", left: "23%", offset: -20 },
+      egg: { width: "50%", left: "23%", offset: 0 },
+      ketchup: { width: "48%", left: "24%", offset: -30 },
+      meat: { width: "55%", left: "23%", offset: -40 },
+      pickle: { width: "50%", left: "25%", offset: 0 },
+      tomato: { width: "45%", left: "25%", offset: 0 },
+      salad: { width: "50%", left: "25%", offset: 0 },
+      plate: { width: "50%", left: "23%", offset: 30 }
+    };
+    this.sizeBurger = 30;
+  }
 
-    constructor() {
-        this.servingDiv = document.getElementById("serving");
-        this.burgerElement = []; 
-        this.positionInfo = {
-            bredTop : { width : "50%" , left : "23%" , offset : 10 },
-            bredBottom : { width : "47%" , left : "23%" , offset : 30 },
-            meat : { width : "50%" , left : "25%" , offset : 0 },
-        }
-        this.sizeBurger = 70;
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async addIngredient(document, interval, margin) {
+    for (var m = 500; m > margin; m -= 10) {
+      await this.moveIngredient(m, document, interval);
     }
+  }
 
-    prepare(listElement){
-        console.log("prepare")
+  async moveIngredient(margin, document, timing) {
+    await this.sleep(timing);
+    document.style.paddingBottom = margin.toString(10) + "px";
+  }
 
-        var tmp = 0;
+  async prepare(listElement, speed) {
+    console.log(listElement);
 
-        var emptyDiv = document.createElement('div');
-        this.servingDiv.appendChild(emptyDiv); 
-        emptyDiv.position = "relative"
-        this.burgerElement.push(emptyDiv);
+    var tmp = 0;
 
-        for(var  ingredient  of listElement){
-            var imageIngredient = document.createElement('img');
-            this.servingDiv.appendChild(imageIngredient);
-            imageIngredient.style.position = "absolute";
-            imageIngredient.style.width = this.positionInfo[ingredient].width;
-            imageIngredient.style.left = this.positionInfo[ingredient].left;  
-            imageIngredient.style.bottom = "0px";  
-            imageIngredient.style.paddingBottom = (this.positionInfo[ingredient].offset + tmp*this.sizeBurger).toString(10)+ "px";           
-            imageIngredient.src = './ressources/ingredients/'+ingredient+".gif";
-            this.burgerElement.push(imageIngredient);
+    var emptyDiv = document.createElement("div");
+    this.servingDiv.appendChild(emptyDiv);
+    emptyDiv.position = "relative";
+    this.burgerElement.push(emptyDiv);
 
-            tmp+= 1;
-        }
+    var imageIngredient = document.createElement("img");
+    this.servingDiv.appendChild(imageIngredient);
+    imageIngredient.style.position = "absolute";
+    imageIngredient.style.width = this.positionInfo["plate"].width;
+    imageIngredient.style.left = this.positionInfo["plate"].left;
+    imageIngredient.style.bottom = "0px";
+    imageIngredient.style.paddingBottom =
+      (this.positionInfo["plate"].offset + tmp * this.sizeBurger).toString(10) +
+      "px";
+    imageIngredient.src = "./ressources/ingredients/plate.gif";
+    this.burgerElement.push(imageIngredient);
+
+    for (var ingredient of listElement) {
+      var imageIngredient = document.createElement("img");
+      this.servingDiv.appendChild(imageIngredient);
+      imageIngredient.style.position = "absolute";
+      imageIngredient.style.width = this.positionInfo[ingredient].width;
+      imageIngredient.style.left = this.positionInfo[ingredient].left;
+      imageIngredient.style.bottom = "0px";
+      imageIngredient.src = "./ressources/ingredients/" + ingredient + ".gif";
+      await this.addIngredient(
+        imageIngredient,
+        speed,
+        this.positionInfo[ingredient].offset + tmp * this.sizeBurger
+      );
+      this.burgerElement.push(imageIngredient);
+      tmp++;
     }
-}
-
-function test(){
-    var Burger = new Burger();
-    burger.prepare(["bredBottom", "meat" , "bredTop"])
-}
+  }
+};
