82668fe0497a68239e0e9c9f69a32210fe8ba6f7
jjoulia <jjeeaann2015@gmail.com>
2020-01-30 14:20:09 +0100

    ingredient are displayed

--- reserve.js
+++ reserve.js
@@ -1,9 +1,41 @@
 var Reserve = class Reserve {
-  constructor() {}
+  constructor(ingredient, unlock, speed, document) {
+    this.speedOfDelivery = speed;
+    this.unlock = unlock;
+    this.ingredient = ingredient;
+    this.document = document;
+    this.listElement = [];
+
+    this.elementInfo = {
+      bredTop: { nb: 10 },
+      meat: { nb: 10 },
+      ketchup: { nb: 10 },
+      salad: { nb: 10 },
+      cheese: { nb: 10 },
+      pickle: { nb: 10 },
+      tomato: { nb: 10 },
+      beacon: { nb: 10 },
+      bredTopBlack: { nb: 10 },
+      egg: { nb: 10 }
+    };
+  }
 
   sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  init() {}
+  init() {
+    if (this.unlock) {
+      for (var i = 0; i < this.elementInfo[this.ingredient].nb; i++) {
+        var imageIngredient = document.createElement("img");
+        imageIngredient.src =
+          "ressources/ingredients/" + this.ingredient + ".gif";
+        imageIngredient.classList.add("foodElement");
+        this.document.appendChild(imageIngredient);
+        this.listElement.push(imageIngredient);
+      }
+    } else {
+      this.document.style.backgroundColor = "grey";
+    }
+  }
 };
--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 var burger;
+var foodList = [];
+var ingredientChart = {
+  bredTop: { unlock: true },
+  meat: { unlock: true },
+  ketchup: { unlock: false },
+  salad: { unlock: false },
+  cheese: { unlock: true },
+  pickle: { unlock: false },
+  tomato: { unlock: false },
+  beacon: { unlock: false },
+  bredTopBlack: { unlock: false },
+  egg: { unlock: false }
+};
 
 function startGame() {
   burger = new Burger();
   burger.init();
+
+  var foodDiv = document.getElementsByClassName("food");
+  for (var food of foodDiv) {
+    var foodElement = new Reserve(
+      food.id,
+      ingredientChart[food.id].unlock,
+      10,
+      food
+    );
+    foodElement.init();
+    foodList.push(foodElement);
+    console.log(food.id);
+  }
 }
 
 function generateCommand() {
